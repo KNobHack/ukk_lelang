c05cc67b06a68be0c0a7e1d43f7ae18bae331282
KNobHack <akudandia7272@gmail.com>
2020-02-15 20:59:43 +0700

    migrasi tabel lelang, genre, asset_gerne

---
+++ database/migrations/2020_02_15_131614_create_lelang_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLelangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lelang', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('asset_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('asset_id')->references('id')->on('assets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('lelang', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['asset_id']);
+        });
+        Schema::dropIfExists('lelang');
+    }
+}
---
+++ database/migrations/2020_02_15_133102_create_asset_genre_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetGenreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asset_genre', function (Blueprint $table) {
+            $table->unsignedBigInteger('asset_id');
+            $table->unsignedBigInteger('genre_id');
+
+            $table->foreign('asset_id')->references('id')->on('assets');
+            $table->foreign('genre_id')->references('id')->on('genres');
+
+            $table->primary(['asset_id', 'genre_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('asset_genre', function (Blueprint $table) {
+            $table->dropForeign(['asset_id']);
+            $table->dropForeign(['genre_id']);
+        });
+        Schema::dropIfExists('asset_genre');
+    }
+}
