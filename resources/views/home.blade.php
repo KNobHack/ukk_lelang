b812f25e3e73e97619577840b21de3c87202fabd
KNobHack <akudandia7272@gmail.com>
2020-02-17 20:13:38 +0700

    New view users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
+
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
