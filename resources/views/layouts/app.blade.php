8a806579302091fc3ea3e0f7e89e7582866c48ce
KNobHack <akudandia7272@gmail.com>
2020-02-17 20:19:25 +0700

    New view users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,7 +63,10 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ url('/home') }}">Home</a>
-                                    <a class="dropdown-item" href="{{ url('/profile') }}">Profile</a>
+                                    <a class="dropdown-item" href="{{ url('/u/' . Auth::user()->id) }}">Profile</a>
+                                    @if(Auth::user()->is_admin)
+                                        <a class="dropdown-item" href="{{ url('/u') }}">Users</a>
+                                    @endif
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
